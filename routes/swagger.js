2e6a4513bb9eecb867d27d9821c942e209161f98
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2025-10-24 15:09:53 -0600

    feat: Add validation and error handling to the API docs route

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
+const validation = require('../middleware/validate');
+const { handleErrors } = require('../middleware/error');
+
 
 router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.get('/api-docs', validation.validateGetAll, handleErrors(swaggerUi.setup(swaggerDocument)));
 
 module.exports = router;
