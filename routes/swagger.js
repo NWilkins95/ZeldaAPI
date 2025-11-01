bb847ccb34e76c8400c50c6fb592be6a09f2c685
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2025-10-31 20:59:43 -0600

    feat: Apply authentication middleware to characters and swagger routes

--- routes/swagger.js
+++ routes/swagger.js
@@ -3,7 +3,10 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 const validation = require('../middleware/validate');
 const { handleErrors } = require('../middleware/error');
+const { checkLogin } = require('../utilities');
 
+// Apply authentication check middleware to all routes in this router
+router.use(checkLogin);
 
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', validation.validateGetAll, handleErrors(swaggerUi.setup(swaggerDocument)));
